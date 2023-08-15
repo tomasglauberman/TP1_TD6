b80ff09873930300ce31dcc87a5c7784e5766526
Juan Silvestri <silvestrijuanignacio@gmail.com>
2023-08-15 10:38:13 -0300

    transformacion datos

--- src/exp1.R
+++ src/exp1.R
@@ -9,6 +9,7 @@ N_BINS <- 10        # Define the number of bins for discretization
 RERUN_EXP <- TRUE   # Set the option to rerun the experiment
 
 # Load provided functions
+set.seed(589115021)
 source("provided_functions.R")
 
 #' Run an experiment to evaluate the performance of a predictive model under different conditions.
@@ -26,13 +27,13 @@ source("provided_functions.R")
 run_experiment <- function(datasets_to_pred, filepath) {
   exp_results <- list()  # Store experiment results
   i <- 1  # Initialize counter for experiment results
-
+  
   # Iterate through different dataset, imputation, and proportion of missing values combinations
   for (dtp in datasets_to_pred) {
     for (impute in c("Yes", "No")) {
-      for (prop_NAs in c(0, 0.7)) {
+      for (prop_NAs in seq(0.1, 0.9, 0.2)) {
         print(c(dtp$dataset_name, impute, prop_NAs))
-
+        
         # Configure preprocessing options based on imputation choice
         if (impute == "Yes") {
           preprocess_control <- list(
@@ -57,7 +58,7 @@ run_experiment <- function(datasets_to_pred, filepath) {
             prop_switch_y=0
           )
         }
-
+        
         # Perform the experiment for the current settings
         if (PARALLELIZE == TRUE) {
           res_tmp <- est_auc_across_depths(dtp, preprocess_control,
@@ -68,7 +69,7 @@ run_experiment <- function(datasets_to_pred, filepath) {
                                                   max_maxdepth=30, prop_val=0.25,
                                                   val_reps=30)
         }
-
+        
         res_tmp$IMPUTED <- impute
         res_tmp$prop_NAs <- prop_NAs
         exp_results[[i]] <- res_tmp
@@ -77,10 +78,10 @@ run_experiment <- function(datasets_to_pred, filepath) {
       }
     }
   }
-
+  
   # Combine experiment results into a single data frame
   exp_results <- do.call(rbind, exp_results)
-
+  
   # Save experiment results to a file
   write.table(exp_results, filepath, row.names=FALSE, sep="\t")
 }
@@ -102,12 +103,12 @@ run_experiment <- function(datasets_to_pred, filepath) {
 plot_exp_results <- function(filename_exp_results, filename_plot, width, height) {
   # Load experiment results
   exp_results <- read.table(filename_exp_results, header=TRUE, sep="\t")
-
+  
   # Calculate mean AUC values for different groups of experimental results
   data_for_plot <- exp_results %>%
     group_by(dataset_name, prop_NAs, IMPUTED, maxdepth) %>%
     summarize(mean_auc=mean(auc), .groups='drop')
-
+  
   # Create a ggplot object for the line plot
   g <- ggplot(data_for_plot, aes(x=maxdepth, y=mean_auc, color=IMPUTED)) +
     geom_line() +
@@ -119,7 +120,7 @@ plot_exp_results <- function(filename_exp_results, filename_plot, width, height)
           panel.grid.major=element_blank(),
           strip.background=element_blank(),
           panel.border=element_rect(colour="black", fill=NA))
-
+  
   # Save the plot to a file
   ggsave(filename_plot, g, width=width, height=height)
 }
@@ -127,14 +128,15 @@ plot_exp_results <- function(filename_exp_results, filename_plot, width, height)
 # Load the datasets
 datasets_to_pred <- list(
   load_df("./data/customer_churn.csv", "Churn", "churn"), # Source: https://archive.ics.uci.edu/dataset/563/iranian+churn+dataset
-  load_df("./data/heart.csv", "Heart", "HeartDisease")    # Source: https://www.kaggle.com/datasets/arnabchaki/data-science-salaries-2023
+  load_df("./data/heart.csv", "Heart", "HeartDisease"),    # Source: https://www.kaggle.com/datasets/arnabchaki/data-science-salaries-2023
+  load_df("./data/sleep_health_proc.csv", "Sleep", "Sleep Disorder")
 )
 
 # Run the experiment
 if (RERUN_EXP ==  TRUE) {
-  run_experiment(datasets_to_pred, "./outputs/tables/sample_exp.txt")
+  run_experiment(datasets_to_pred, "./outputs/tables/exp1.txt")
 }
 
 # Plot the experiment results
-plot_exp_results( "./outputs/tables/sample_exp.txt", "./outputs/plots/sample_exp.jpg", width=5, height=4)
+plot_exp_results( "./outputs/tables/exp1.txt", "./outputs/plots/exp1.jpg", width=5, height=4)
 
